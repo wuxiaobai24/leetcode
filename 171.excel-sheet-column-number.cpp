8a1f792cd2c4d9743080d42fd2d4f58eb8c50a02
wuxiaobai24 <wuxiaobai24@foxmail.com>
2019-01-09 20:06:30 +0800

    171 AC

---
+++ 171.excel-sheet-column-number.cpp
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=171 lang=cpp
+ *
+ * [171] Excel Sheet Column Number
+ *
+ * https://leetcode.com/problems/excel-sheet-column-number/description/
+ *
+ * algorithms
+ * Easy (50.44%)
+ * Total Accepted:    200.2K
+ * Total Submissions: 396.7K
+ * Testcase Example:  '"A"'
+ *
+ * Given a column title as appear in an Excel sheet, return its corresponding
+ * column number.
+ * 
+ * For example:
+ * 
+ * 
+ * ⁠   A -> 1
+ * ⁠   B -> 2
+ * ⁠   C -> 3
+ * ⁠   ...
+ * ⁠   Z -> 26
+ * ⁠   AA -> 27
+ * ⁠   AB -> 28 
+ * ⁠   ...
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: "A"
+ * Output: 1
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: "AB"
+ * Output: 28
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: "ZY"
+ * Output: 701
+ * 
+ */
+class Solution {
+public:
+    int titleToNumber(string s) {
+        int res = 0;
+        for(int i = 0;i < s.size(); i++) {
+            int v = (int)(s[i] - 'A') + 1;
+            res = res * 26 + v;
+        }
+        return res;
+    }
+};
